ba45210de8325676f30c6dda0b0230a25e390369
me <me>
2024-03-15 23:43:37 +0100

    handler

--- PowerShell/PwsSnippets.cs
+++ PowerShell/PwsSnippets.cs
@@ -2,6 +2,8 @@
 
 
 
+using System;
+
 namespace PwShell;
 
 
@@ -11,14 +13,23 @@ public class PwsSnippets
     public string ValidateOriginOrThrow { get; init; }
     public string ValidateBranchOrThrow { get; init; }
     public string GitPush { get; init; }
+    public string ChangeToHookDirectory { get; init; }
+    public string ChangeToActionBranch { get; init; }
+    public string WriteToLogFile { get; init; }
+    public string GitAdd { get; init; }
+    public string GitCommit { get; init; }
 
-    public PwsSnippets(string urlOfOrigin, string branch)
+    public PwsSnippets(string urlOfOrigin, string branch, string absoluteHookDirectoryPath, string actionBranch)
     {
         IsGitRepo = "$ignored = git branch";
         ValidateOriginOrThrow = GetValidateOriginOrThrow(urlOfOrigin);
         ValidateBranchOrThrow = GetValidateBranchOrThrow(branch);
-        GitPush = "git push";
-
+        GitPush = "$ignored = git push";
+        ChangeToHookDirectory = $"cd {absoluteHookDirectoryPath}";
+        ChangeToActionBranch = GetChangeToActoinBranch(actionBranch);
+        WriteToLogFile = $"echo {DateTime.Now} > logfile.txt";
+        GitAdd = "$ignored = git add .";
+        GitCommit = "$ignored = git commit -m GitAndDeploy";
     }
 
 
@@ -32,6 +43,15 @@ public class PwsSnippets
     private static string GetValidateBranchOrThrow(string branch)
     {
         return $@"$b=git branch; if(!($b -Match '\*\s{branch}')){_curlyOpen}Write-Error -Message 'Bad branch'{_curlyClose}";
+    }
+
+    private static string GetChangeToActoinBranch(string actionBranch)
+    {
+        //var insideCatch = $"if(!($_.Exeption.Message -Match 'Already on')){_curlyOpen}Write-Error -Message $_.Exeption.Message{_curlyClose}";
+        var insideCatch = $"echo $_.Exeption.Message";
+        //return $"try{_curlyOpen}git checkout {actionBranch}{_curlyClose} catch {_curlyOpen + insideCatch + _curlyClose}";
+        //return $"try{_curlyOpen}echo hi; git checkout test; echo hiagain;{actionBranch}{_curlyClose} catch {_curlyOpen + "echo insideCatch;" + insideCatch + _curlyClose}";
+        return $@"$b=git branch; if(!($b -Match '\*\s{actionBranch}')){_curlyOpen}git checkout {actionBranch}{_curlyClose}";
     }
 }
 
